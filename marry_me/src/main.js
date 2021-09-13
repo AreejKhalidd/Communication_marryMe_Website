e2c7f8fa64601ba6355054ac83b636845a797d90
OmniaFares <omniafares133@gmail.com>
2021-09-13 14:15:57 +0200

    trying different pacakages for chat ui

--- marry_me/src/main.js
+++ marry_me/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router';
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const routes=[
+  {path:"/listChats" , component:require('./components/listChats.vue').default},
+    {path:"/Chat/:id/:name" , component:require('./components/Chat.vue').default},
+    {path:"/Chat2/:id/:name" , component:require('./components/Chat2.vue').default},
+]
+const router = new VueRouter({
+    routes:routes,
+    mode:"history"
+})
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
