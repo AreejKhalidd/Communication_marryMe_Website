470b3a86ee79242fd840159c7a0905e93eda7eeb
abdel <cairooo119@gmail.com>
2021-09-13 21:26:35 +0200

    change language in block page and create fav page with hi backend

--- marry_me/src/router/index.js
+++ marry_me/src/router/index.js
@@ -3,41 +3,47 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import MyProfile from '../views/MyProfile.vue'
 import block from '../views/Block'
+import friend from '../views/Friend'
 
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/my_profile',
-    name: 'MyProfile',
-    component: MyProfile
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/my_profile',
+        name: 'MyProfile',
+        component: MyProfile
+    },
 
-  {
-    path: '/list',
-    name: 'block',
-    component: block
-  },
+    {
+        path: '/blockedUsers',
+        name: 'block',
+        component: block
+    },
+    {
+        path: '/favUsers',
+        name: 'friend',
+        component: friend
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
--- marry_me/src/views/Block.vue
+++ marry_me/src/views/Block.vue
@@ -1,9 +1,9 @@
 <template>
     <div data-app>
-        <v-toolbar  style="background-color: #FE6265;color: black">
+        <v-toolbar  style="background-color: #FE6265;color: black;direction: rtl">
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-            <v-toolbar-title>Blocked Users</v-toolbar-title>
+            <v-toolbar-title>قائمة المحظورين</v-toolbar-title>
 
             <v-spacer></v-spacer>
         </v-toolbar>
@@ -29,7 +29,7 @@
 },
     mounted() {
     // GET request using axios with set headers
-    const AuthStr = 'Bearer '.concat("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTQ5MDUxOSwiZXhwIjoxNjMxNDk0MTE5LCJuYmYiOjE2MzE0OTA1MTksImp0aSI6Ik03VjhUN2RSWDlnakpNT0oiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.RnFRNqZOv1MGaUWyhqbKVkCgDkfUMytEwwEXgRyeo_k");
+    const AuthStr = 'Bearer '.concat("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTU1NzI0MiwiZXhwIjoxNjMxNTYwODQyLCJuYmYiOjE2MzE1NTcyNDIsImp0aSI6IlhlZ0R6cXlEOGxvdlRONm0iLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.O_MQdJI3mAyg2BxoO-KuSPuFIuxfdoU1UvCUP0Dnymc");
     axios.get("http://127.0.0.1:8000/api/getAllBlocks", {headers: {Authorization: AuthStr}})
     .then(response => {
     // If request is good...
