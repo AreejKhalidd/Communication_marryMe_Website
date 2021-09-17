e265d8b7160572d7646068bc60554f1d916d4985
aya1sameh <49209317+aya1sameh@users.noreply.github.com>
2021-09-17 16:41:26 +0200

    Adding new package and updating the chat
    1-Adding package-lock.json to .gitignore
    2-Adding moment package
    3-updating the chat vue

--- marry_me/src/views/Chat.vue
+++ marry_me/src/views/Chat.vue
@@ -16,106 +16,134 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import ChatWindow from 'vue-advanced-chat'
-  import 'vue-advanced-chat/dist/vue-advanced-chat.css'
+import axios from "axios";
+import ChatWindow from "vue-advanced-chat";
+import "vue-advanced-chat/dist/vue-advanced-chat.css";
+import moment from "moment";
 
-  export default {
-    components: {
-      ChatWindow
+export default {
+  components: {
+    ChatWindow,
+  },
+  data() {
+    return {
+      rooms: [],
+
+      messages: [],
+
+      currentUserId: "",
+      currentUserName: "",
+      currentUserAvatar: "",
+      currentUserStatus: "",
+
+      Vip: false,
+      type: "image/*",
+      AllRoomsAreLoaded: false,
+      AllmsgsAreLoaded: false,
+      OurTheme: {
+        general: {
+          color: "#FF6265",
+        },
+      },
+    };
+  },
+  methods: {
+    getUserInfo() {
+      //if (localStorage.getItem('usertoken') === null) this.$router.push('/');
+      //const option = { headers: { Authorization: `${'Bearer'} ${localStorage.getItem('usertoken')}` } };//waiting for the login to be finished to store the access token
+      const option = {
+        headers: {
+          Authorization: `${"Bearer"} ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTg2ODg2MCwiZXhwIjoxNjMxODk3NjYwLCJuYmYiOjE2MzE4Njg4NjAsImp0aSI6IktpMkh2ZmVtc3BYYWN3TEEiLCJzdWIiOjQsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.u9HB6NZeQY8q1eLyC7EMpAG783c4gZxScUH9x0aNwUI"}`,
+        },
+      }; //temp for testing the request
+      axios
+        .get("http://127.0.0.1:8000/api/profile", option)
+        .then((response) => {
+          this.currentUserId = response.data.id;
+          this.currentUserName = response.data.name;
+          this.currentUserAvatar = response.data.image;
+          if (response.data.VIP == 1) {
+            this.Vip = true;
+          }
+          if (response.data.online == 1) {
+            this.currentUserStatus = "online";
+          } else {
+            this.currentUserStatus = "offline";
+          }
+        });
+      this.AllRoomsAreLoaded = true;
     },
-    data() {
-      return {
-        rooms: [],  
+    Chats() {
+      //if (localStorage.getItem('usertoken') === null) this.$router.push('/');
+      //const option = { headers: { Authorization: `${'Bearer'} ${localStorage.getItem('usertoken')}` } };//waiting for the login to be finished to store the access token
+      const option = {
+        headers: {
+          Authorization: `${"Bearer"} ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTg2ODg2MCwiZXhwIjoxNjMxODk3NjYwLCJuYmYiOjE2MzE4Njg4NjAsImp0aSI6IktpMkh2ZmVtc3BYYWN3TEEiLCJzdWIiOjQsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.u9HB6NZeQY8q1eLyC7EMpAG783c4gZxScUH9x0aNwUI"}`,
+        },
+      }; //temp for testing the request
+      axios
+        .get("http://127.0.0.1:8000/api/listallchats", option)
+        .then((response) => {
+          const temprooms = [];
+          let tempusers = [];
+          let DateTime = "";
+          let sent = false;
+          let delivered = false;
+          let seenmsg = false;
+          let newmsg = false;
 
-        unreadCount:[],
-        index:[],
-        lastMessage:[],
-        content:[],
-        senderId:[],
-        username:[],
-        timestamp:[],
-        saved:[],
-        distributed:[],
-        seen:[],
-        new:[],
-
-        messages: [],
-        currentUserId: "",
-
-        currentUserName:"",
-        currentUserAvatar:"",
-        currentUserStatus:"",
-        Vip:false,
-        type:"image/*",
-        AllRoomsAreLoaded:false,
-        AllmsgsAreLoaded:false,
-        OurTheme:{
-            general: {
-                color: '#FF6265'
+          for (let i = 0; i < response.data.length; i++) {
+            tempusers.push(
+              {
+                _id: `${this.currentUserId}`,
+                username: `${this.currentUserName}`,
+                avatar: `http://127.0.0.1:8000${this.currentUserAvatar}`,
+              },
+              {
+                _id: `${response.data[i].user_id}`,
+                username: `${response.data[i].name}`,
+                avatar: `http://127.0.0.1:8000${response.data[i].image}`,
+              }
+            );
+            DateTime = moment(response.data[i].created_at).format("D/M/YYYY H:mm A");
+            if (response.data[i].status == 0) {
+              sent=true;
+              delivered=true;//temp to be handle in backend
+              newmsg=true;
             }
-        }
-        
-      }
-    },
-    methods: {
-        getUserInfo() {
-            //if (localStorage.getItem('usertoken') === null) this.$router.push('/');
-            //const option = { headers: { Authorization: `${'Bearer'} ${localStorage.getItem('usertoken')}` } };//waiting for the login to be finished to store the access token
-            const option = { headers: { Authorization: `${'Bearer'} ${'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTg2ODg2MCwiZXhwIjoxNjMxODk3NjYwLCJuYmYiOjE2MzE4Njg4NjAsImp0aSI6IktpMkh2ZmVtc3BYYWN3TEEiLCJzdWIiOjQsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.u9HB6NZeQY8q1eLyC7EMpAG783c4gZxScUH9x0aNwUI'}` } };//temp for testing the request
-            axios.get('http://127.0.0.1:8000/api/profile', option)
-              .then((response) => {
-                this.currentUserId=response.data.id;
-                this.currentUserName=response.data.name;
-                this.currentUserAvatar=response.data.image;
-                if(response.data.VIP==1){
-                    this.Vip=true;
-                }
-                if(response.data.online==1){
-                    this.currentUserStatus="online";
-                }
-                else{
-                    this.currentUserStatus="offline";
-                }
+            else if(response.data[i].status == 1){
+              seenmsg=true;
+            }
+            //to be updated with the last msg timestamp to put it as index for the chats to be sorted
+            temprooms.push({
+              roomId: `${response.data[i].chat_id}`,
+              roomName: `${response.data[i].name}`,
+              avatar: `http://127.0.0.1:8000${response.data[i].image}`,
+              unreadCount: `${response.data[i].unreadcount}`,
+              index: `${response.data[i].created_at}`,
+              lastMessage: {
+                content: `${response.data[i].content}`,
+                senderId: `${response.data[i].sender_id}`,
+                username: `${response.data[i].sender_name}`,
+                timestamp: `${DateTime}`,
+                saved:sent,
+                distributed:delivered,
+                seen:seenmsg,
+                new:newmsg,
+              },
+              users: tempusers,
             });
-            this.AllRoomsAreLoaded=true;
-        },
-        Chats(){
-            //if (localStorage.getItem('usertoken') === null) this.$router.push('/');
-            //const option = { headers: { Authorization: `${'Bearer'} ${localStorage.getItem('usertoken')}` } };//waiting for the login to be finished to store the access token
-            const option = { headers: { Authorization: `${'Bearer'} ${'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMTg2ODg2MCwiZXhwIjoxNjMxODk3NjYwLCJuYmYiOjE2MzE4Njg4NjAsImp0aSI6IktpMkh2ZmVtc3BYYWN3TEEiLCJzdWIiOjQsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.u9HB6NZeQY8q1eLyC7EMpAG783c4gZxScUH9x0aNwUI'}` } };//temp for testing the request
-            axios.get('http://127.0.0.1:8000/api/listallchats', option)
-              .then((response) => {
-                const temprooms=[];
-                var tempusers=[];
-                //var tempseen=false;
-                for (let i = 0; i < response.data.length; i++) {
-                    tempusers.push({
-                    _id:`${this.currentUserId}`,
-                     username:`${this.currentUserName}`,
-                     avatar:`http://127.0.0.1:8000${this.currentUserAvatar}`,
-                    },{
-                    _id:`${response.data[i].user_id}`,
-                     username:`${response.data[i].name}`,
-                     avatar:`http://127.0.0.1:8000${response.data[i].image}`,
-                    });
-                    //to be updated with the last msg timestamp to put it as index for the chats to be sorted
-                    temprooms.push({roomId:`${response.data[i].chat_id}`, roomName:`${response.data[i].name}`, avatar:`http://127.0.0.1:8000${response.data[i].image}`,unreadCount:`${response.data[i].unreadcount}`, index:`${response.data[i].created_at}`,users:`${tempusers}`});
-                    console.log(tempusers);
-                    tempusers=[];
-                    //tempseen=false;
-                }
-                //console.log(users);
-                console.log(temprooms);
-                this.rooms=temprooms;
-            });
-
-        },
-        
+            tempusers = [];
+            //tempseen=false;
+          }
+          //console.log(users);
+          this.rooms = temprooms;
+        });
     },
-    created() {
-            this.getUserInfo();
-            this.Chats();
-    },
-  }
+  },
+  created() {
+    this.getUserInfo();
+    this.Chats();
+  },
+};
 </script>
