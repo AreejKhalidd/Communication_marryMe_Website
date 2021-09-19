6e3daf8c7fdbf40d73b09f50c05d59ebb4253b83
OmniaFares <omniafares133@gmail.com>
2021-09-20 00:16:14 +0200

    fix certifyme request

--- marry_me/src/views/CertifyMe.vue
+++ marry_me/src/views/CertifyMe.vue
@@ -13,8 +13,7 @@
             <h5 class="text-center">...قم برفع صور تتضمن معلومات تريد تصديقها</h5>
             <br>
                     <v-file-input 
-                    v-model="file"
-                    ref="myfile"
+                    v-model="files"
                     accept="image/*"
                     prepend-icon="mdi-camera-plus"
                     color="#FF6265"
@@ -51,73 +50,28 @@ export default {
    },
   data() {
    return {
-        file:'',
-        files:null,
-       certifyMsg:'',
+       files:[],
        boolean: false
    }
   },
-  methods: {
-      certifyme(){
-          const token = 'Bearer '.concat("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMjA3OTU5MywiZXhwIjoxNjMyMTA4MzkzLCJuYmYiOjE2MzIwNzk1OTMsImp0aSI6InRPc3NTUjZzU0RVWjU4SkUiLCJzdWIiOjExLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.TpjIO5us9-5-lcKgJ2hefFcniCHRYEGnk0hDoeSSOes");
-                axios({
-                method: 'post',
-                url: "http://127.0.0.1:8000/api/certified",
-                headers: {Authorization: token},
-                data: {image :this.file}
-                }).then(response => {
-                this.certifyMsg=response.data.message;
-                this.boolean=!this.boolean;
-                console.log(this.file);
-                    })
-                            .catch((error) => {
-                            console.log('There is error:'+error);
-                            alert(error)
-                            return "error occoured"
-                    });
-
-      },
-      submitFiles() {
-         const token = 'Bearer '.concat("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMjA3OTU5MywiZXhwIjoxNjMyMTA4MzkzLCJuYmYiOjE2MzIwNzk1OTMsImp0aSI6InRPc3NTUjZzU0RVWjU4SkUiLCJzdWIiOjExLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.TpjIO5us9-5-lcKgJ2hefFcniCHRYEGnk0hDoeSSOes");
-          if (this.files) {
-              let formData = new FormData();
-              for (let file of this.files) {
-                  formData.append("files", file, file.name);
-                  console.log(file);
-              }
-              axios
-                  .post("http://127.0.0.1:8000/api/certified", formData,{headers: {
-                  'accept': 'application/json',
-                  'Accept-Language': 'en-US,en;q=0.8',
-                  'Content-Type': `multipart/form-data`,
-                  'Authorization': token
-                   }
-                })
-                  .then(response => {
-                      console.log("Success!");
-                      console.log({ response });
-                      console.log(...formData );
-                  })
-                  .catch(error => {
-                      console.log({ error });
-                  });
-          } else {
-              console.log("there are no files.");
+  methods: {                  
+       Validate() {
+          if (this.$refs.form.validate()) {
+            const fd = new FormData();
+            for (let file of this.files) {
+                    fd.append("image[]", file);
+            }
+            const option = { headers: { Authorization: `${'Bearer'} ${'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMjA3OTU5MywiZXhwIjoxNjMyMTA4MzkzLCJuYmYiOjE2MzIwNzk1OTMsImp0aSI6InRPc3NTUjZzU0RVWjU4SkUiLCJzdWIiOjExLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.TpjIO5us9-5-lcKgJ2hefFcniCHRYEGnk0hDoeSSOes'}`,'Content-Type': 'multipart/form-data' } };//temp for testing the request
+            axios.post('http://127.0.0.1:8000/api/certified', fd, option)
+              .then((response) => {
+                console.log(response.data.message);
+                this.boolean = true;
+              })
+              .catch(() => {
+                console.log("error occured");
+              });
           }
       },
-       Validate() {
-      if (this.$refs.form) {
-        const fd = new FormData();
-        fd.append('image', this.file);
-        const option = { headers: { Authorization: `${'Bearer'} ${'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYzMjA3OTU5MywiZXhwIjoxNjMyMTA4MzkzLCJuYmYiOjE2MzIwNzk1OTMsImp0aSI6InRPc3NTUjZzU0RVWjU4SkUiLCJzdWIiOjExLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.TpjIO5us9-5-lcKgJ2hefFcniCHRYEGnk0hDoeSSOes'}`,'Content-Type': 'multipart/form-data' } };//temp for testing the request
-        axios.post('http://127.0.0.1:8000/api/certified', fd, option)
-          .then((response) => {
-            console.log(response.data.message);
-          })
-          .catch(() => {
-          });
-      }
-    },
       
   },
 };
