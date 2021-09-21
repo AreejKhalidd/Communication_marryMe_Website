521c8edc6d2272a6e59d7c83a03b958125096042
amirafathy-59 <amirafa592001@gmail.com>
2021-09-21 20:35:28 +0100

    Auth Pages

---
+++ marry_me/src/router/index.js
@@ -0,0 +1,140 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import MyProfile from '../views/MyProfile.vue'
+import block from '../views/Block'
+import friend from '../views/Friend'
+import HomePage from '../views/HomePage'
+import Userinfo from '../views/Userinfo'
+import follower from '../views/LikedMe'
+import Chat from '../views/Chat'
+import CertifyMe from '../views/CertifyMe'
+import requests from '../views/Request'
+import questions from '../views/Quizzes'
+import SearchResult from '../views/SearchResult'
+import Userinfoforsearch from '../views/Userinfoforsearch'
+<<<<<<< HEAD
+import Login from '../views/Login';
+import ResetPassword from '../views/ResetPassword';
+import Register from '../views/Register';
+import ForgotPassword from '../views/ForgotPassword';
+=======
+import UserProfile from '../components/Profile'
+>>>>>>> 649419f26f26b7a681a1c7818a48c29dd9c8cbe9
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/my_profile',
+        name: 'MyProfile',
+        component: MyProfile
+    },
+
+    {
+        path: '/blockedUsers',
+        name: 'block',
+        component: block
+    },
+    {
+        path: '/favUsers',
+        name: 'friend',
+        component: friend
+    },
+    {
+        path: '/homepage',
+        name: 'HomePage',
+        component: HomePage
+    },
+    {
+        path: '/followersList',
+        name: 'follower',
+        component: follower
+    },
+    {
+        path: '/userinfo',
+        name: 'Userinfo',
+        component: Userinfo,
+        props: true
+    },
+    {
+        path: '/chat',
+        name: 'Chat',
+        component: Chat,
+    },
+    {
+        path: '/certifyme',
+        name: 'CertifyMe',
+        component: CertifyMe,
+    },
+    { 
+        path: '/allRequests',
+        name: 'requests',
+        component: requests
+    },
+    {
+        path: '/questions',
+        name: 'questions',
+        component: questions
+    },
+    {
+        path: '/searchResult',
+        name: 'SearchResult',
+        component: SearchResult,
+        props: true
+    },
+    {
+        path: '/userinfoforsearch',
+        name: 'Userinfoforsearch',
+        component: Userinfoforsearch,
+        props: true
+    },
+    {
+<<<<<<< HEAD
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+    },
+    {
+        path: '/forgotpassword',
+        name: 'ForgotPassword',
+        component: ForgotPassword,
+    },
+    {
+        path: '/resetpassword',
+        name: 'ResetPassword',
+        component: ResetPassword,
+=======
+        path: '/userProfile/:id',
+        name: 'UserProfile',
+        component: UserProfile,
+        props: true
+>>>>>>> 649419f26f26b7a681a1c7818a48c29dd9c8cbe9
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
